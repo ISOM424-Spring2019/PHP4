9c4e7c27f4a9aaff6a76ab7c88ab63b1898e2857
ISOM424-Spring2019 <45808575+ISOM424-Spring2019@users.noreply.github.com>
2018-12-29 08:37:56 -0500

    Add files via upload
    PHP4 .php
    Uploaded 12-29-2019
    By: Peter Treadway

---
+++ php4.php
@@ -0,0 +1,50 @@
+<html>
+
+<head></head>
+
+<body>
+
+<?php echo phpinfo(); ?>
+<?php  
+        // Variables to tune the retry logic.    	
+        $connectionTimeoutSeconds = 30;  // Default of 15 seconds is too short over the Internet, sometimes.  
+        $maxCountTriesConnectAndQuery = 3;  // You can adjust the various retry count values.  
+        $secondsBetweenRetries = 4;  // Simple retry strategy.  
+        $errNo = 0;  
+	//	$serverName = "tcp:s18.winhost.com,1433"; 
+        $serverName = "tcp:s18.winhost.com,1433";  
+    //    
+	$connectionOptions =   
+           array("UID"=>"DB_97863_as_user", "PWD"=>"Greendream3", "DATABASE"=>"DB_97863_as");  
+        $conn = sqlsrv_connect( $serverName, $connectionOptions);
+            if ($conn == true) 
+			   {  
+                echo "Connection was established";  
+                echo "<br>";
+				                $tsql = "SELECT Username FROM  [DB_97863_as].[dbo].[Users]";  
+                $stmt = sqlsrv_query($conn, $tsql);  
+                if ($stmt == false) {
+                    echo "Error in query execution";  
+                    echo "<br>";  
+                    die(print_r(sqlsrv_errors(), true));  
+                }
+				echo "Milestone 1";  
+                echo "<br>";
+                while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {     
+                    echo $row['Username'] . "<br/>" ;
+                }  
+                sqlsrv_free_stmt($stmt);  
+                sqlsrv_close( $conn); 
+                break; 
+                }
+             else
+                 {
+// print output
+                 echo 'Not connected';
+				 echo "<br>";
+				 die( print_r( sqlsrv_errors(), true));
+                 }
+				
+?>  
+
+
